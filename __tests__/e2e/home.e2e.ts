898f9385d2ed193f60c7c5c9c598a2993450e22b
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2025-10-26 14:47:56 +0600

    fix:testing e2e

--- __tests__/e2e/home.e2e.ts
+++ __tests__/e2e/home.e2e.ts
@@ -3,14 +3,14 @@ import { test, expect } from "@playwright/test";
 test.describe("Home Page", () => {
 
   test("Banner and navigation to shop", async ({ page }) => {
-    await page.goto("https://next-bazaar-ashy.vercel.app/");
+    await page.goto("/");
     await expect(page.locator("text=Featured Products")).toBeVisible();
     await page.click("text=Shop Now");
     await expect(page).toHaveURL(/shop/);
   });
 
   test("Navigation to About page", async ({ page }) => {
-    await page.goto("https://next-bazaar-ashy.vercel.app/");
+    await page.goto("/");
     await page.click("text=About");
     await expect(page).toHaveURL(/about/);
     await expect(page.locator("text=Our Story")).toBeVisible();
